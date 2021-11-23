1239e5c0d283db839a5fef1b16780e548f9d95eb
gitguanqi <jcguanqi@126.com>
2021-11-23 16:47:54 +0800

    style fix

--- assets/js/device.js
+++ assets/js/device.js
@@ -2,20 +2,20 @@
  * @Author: fegq
  * @Date: 2021-09-23 10:36:04
  * @LastEditors: fegq
- * @LastEditTime: 2021-09-23 10:45:18
+ * @LastEditTime: 2021-11-23 15:39:57
  * @Description: This is a file comment!
- * @Version: 
+ * @Version:
  */
 function calcRem() {
-    let clientWidth = document.documentElement.clientWidth ||
-        document.body.clientWidth;
-    if (clientWidth <= 750) {
-        document.documentElement.style.fontSize = (clientWidth / 23.4375) + 'px';
-    }
+  let clientWidth =
+    document.documentElement.clientWidth || document.body.clientWidth;
+  if (clientWidth <= 750) {
+    document.documentElement.style.fontSize = clientWidth / 23.4375 + "px";
+  } else {
+    document.documentElement.style.fontSize = 16 + "px";
+  }
 }
 
 calcRem();
 
-window.addEventListener('resize', function () {
-    calcRem();
-})
+window.addEventListener("resize", calcRem, false);
