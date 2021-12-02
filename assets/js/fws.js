a1546c2bea3afbcc4ba23ee0dab140c5fc8b8a7a
gitguanqi <jcguanqi@126.com>
2021-12-02 09:47:44 +0800

    fix some cdn url

--- assets/js/fws.js
+++
@@ -1,3 +0,0 @@
-let fws = document.createElement('script');
-fws.src = 'https://felab.guanqi.xyz/assets/js/fireworks.js';
-document.body.appendChild(fws);
--- cases/gotonav/js/golink.js
+++ cases/gotonav/js/golink.js
@@ -4,7 +4,7 @@
  * @version: v0.0.1
  * @Date: 2021-05-20 14:51:00
  * @LastEditors: fegq
- * @LastEditTime: 2021-05-20 15:02:33
+ * @LastEditTime: 2021-12-02 08:49:03
  */
 function goLink (header, nav, navbox, content) {  
     let headerHei = $(header).height();
@@ -15,7 +15,7 @@ function goLink (header, nav, navbox, content) {
         navs.length = section.length;
         section.each(function (i, v) {
             let pos = $(v).offset().top;
-            if (pos <= scrollTop - $(navbox).height() + headerHei) {
+            if (pos <= scrollTop + headerHei + 80) {
                 navs.eq(i).addClass('active').siblings().removeClass('active');
             }
         });
@@ -28,7 +28,7 @@ function goLink (header, nav, navbox, content) {
     navs.click(function () {
         let index = $(this).index();
         let topHeight = section.eq(index).offset().top;
-        let top = topHeight - headerHei;
+        let top = topHeight - headerHei - 50;
         $('html,body').animate({'scrollTop': top}, 1000);
         $(this).addClass('active').siblings().removeClass('active');
     })
